fa3f6ff6704c0cf3e842e53af3e6c796d839c5d6
Ruby Learner <rubyland.myanmar.mg@gmail.com>
2021-06-16 16:48:20 +0630

    change grid

--- lib/screen/responseive/responsive.dart
+++ lib/screen/responseive/responsive.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:text_view/api/model/country.dart';
 
@@ -12,7 +13,7 @@ class ListScreen extends StatelessWidget {
       print(size.maxWidth);
       if (size.maxWidth < 700) {
         return Mobile(country);
-      } else if (size.maxWidth > 700 && size.maxWidth < 1000) {
+      } else if (size.maxWidth > 700 && size.maxWidth < 1035) {
         return TabletOrDeskTop(country, false);
       }
       return TabletOrDeskTop(country, true);
@@ -68,52 +69,52 @@ class TabletOrDeskTop extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home Screen"),
       ),
-      body: GridView.builder(
+      body: StaggeredGridView.countBuilder(
+        crossAxisCount: 8,
+
         itemCount: country.length,
-        itemBuilder: (context, pos) {
-          return item(country[pos]);
+        itemBuilder: (BuildContext context, int index) {
+          return item(country[index]);
         },
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: isDesktop ? 5 : 3,
-            childAspectRatio: 0.88
-           ),
-      ),
+        staggeredTileBuilder: (int index) =>
+        new StaggeredTile.count(2,  isDesktop ? 2 : 3 ),
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+      )
     );
   }
 
   Widget item(Country country) {
-    return Expanded(
-      child: Card(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Text('${country.name}'),
-          Text('${country.region}'),
-          Text('${country.population}'),
-          Container(
-            width: 200,
-            height: 200,
-            child: SvgPicture.network(
-              country.flag!,
-              fit: BoxFit.contain,
-              width: 100,
-              height: 100,
-            ),
+    return Card(
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Text('${country.name}'),
+        Text('${country.region}'),
+        Text('${country.population}'),
+        Container(
+          width: 200,
+          height: 200,
+          child: SvgPicture.network(
+            country.flag!,
+            fit: BoxFit.contain,
+            width: 100,
+            height: 100,
           ),
-        ]),
-        // child: ListTile(
-        //   leading: Container(
-        //     width: 100,
-        //     height: 100,
-        //     child: SvgPicture.network(
-        //     country.flag!,
-        //     fit: BoxFit.fill,
-        //     width: 100,
-        //     height: 100,),
-        //   ),
-        //   title: Text('${country.name}'),
-        //   subtitle: Text('${country.region}'),
-        //   trailing: Text('${country.population}'),
-        // ),
-      ),
+        ),
+      ]),
+      // child: ListTile(
+      //   leading: Container(
+      //     width: 100,
+      //     height: 100,
+      //     child: SvgPicture.network(
+      //     country.flag!,
+      //     fit: BoxFit.fill,
+      //     width: 100,
+      //     height: 100,),
+      //   ),
+      //   title: Text('${country.name}'),
+      //   subtitle: Text('${country.region}'),
+      //   trailing: Text('${country.population}'),
+      // ),
     );
   }
 }
